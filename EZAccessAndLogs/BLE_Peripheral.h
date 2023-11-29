8557fc4eadb0011c7d82abf02c07d4eac8084b5c
ericriv88 <ericriv88@gmail.com>
2023-11-28 23:19:51 -0800

    Change to external readers via BLE
    External readers now send their Hash to the reader hub and the reader hub checks it against the specified file. Sends a byte back indicating a grant or deny.

--- EZAccessAndLogs/BLE_Peripheral.h
+++ EZAccessAndLogs/BLE_Peripheral.h
@@ -6,7 +6,10 @@
 #include "BLEService.h"
 #include <ArduinoBLE.h>
 
-#define default_uuid "09B10000-E8F2-537E-4F6C-D104768A1200"
+#define default_uuid "09B10000-E8F2-537E-4F6C-D104768A1000"
+
+String UIDHash = "";
+bool validUID = false;
 
 void BLE_Peripheral_init() {      //initialize BLE_Peripheral 
  Serial.print("Starting BLE peripheral... ");
@@ -33,41 +36,88 @@ void BLE_Peripheral_Add_Reader() {  //adds new reader by making new txt file for
 
 String intToHEX(int num) {   //takes int and makes it a 2 digit hex value
   String ret = String(num, HEX);
-  while(ret.length() < 3)
+  while(ret.length() < 2)
     ret = "0" + ret;
   return ret;
 }
 
-void addCharacteristicsFromFile(BLEService service, String fileName) {  //creates characteristics for each line in UID.txt file and adds them to service with incrementing UUIDs
-  String tempServUUID = service.uuid();
-  tempServUUID.remove((tempServUUID.length() - 1));
-  tempServUUID.remove((tempServUUID.length() - 1));   //get service UUID excluding the last 2 HEX digits
-  for(int i = 0; i < SDLineCount(fileName); i++) {
-    String tempCharacUUID = tempServUUID + intToHEX(i); //define UUID for characterisitc that increments from the service UUID
-    BLEStringCharacteristic customCharacteristic(tempCharacUUID.c_str(), BLERead, 64); // Define a custom characteristic
-    customCharacteristic.writeValue(readSDLine(fileName, (i + 1))); // Initialize the characteristic value
-    service.addCharacteristic(customCharacteristic);  //add characteristic to service
+String readCharacteristicString(BLECharacteristic charac, int length) {
+  uint8_t val[length];
+  charac.readValue(val, length);
+  String ret = "";
+  for(int i = 0; i < length; i++) {
+    ret += char(val[i]);
+  }
+  return ret;
+}
+
+void writeHandle(BLEDevice central, BLECharacteristic characteristic) {   //handles event that a characteristic is written to
+  if(UIDHash.length() != 64) {
+    UIDHash += readCharacteristicString(characteristic, 16);    //adds the 16 byte chunk of the hash to a global string
+    characteristic.writeValue("0000000000000000");              //overwrites characteristic to hide previously sent chunk 
+  }
+  else {
+    UIDHash += readCharacteristicString(characteristic, 1);    //adds the single reader number byte to hash
+    characteristic.writeValue("0");              //overwrites characteristic to hide previously sent chunk
+  }        
+  if(UIDHash.length() == 65) {  //when full hash value received plus reader number
+    String readerNum = String(UIDHash.charAt(UIDHash.length() - 1));  //save reader number for file name
+    String fileName = "READERS/UID" + readerNum + ".txt";
+    UIDHash.remove((UIDHash.length() - 1));   //remove reader number from hash
+    if(checkSDForString(fileName, UIDHash)) validUID = true;  //check if valid from file given reader number
+    UIDHash = ""; //clear Hash string
   }
 }
 
-void BLE_Peripheral_Reset_Services() {  //resets BLE and adds file contents to new services 
+void readHandle(BLEDevice central, BLECharacteristic characteristic) {  //handles event that key characteristic is read
+  if(validUID) {
+    characteristic.writeValue((byte)0x88);  //send 0x88 if sent hash was valid
+    validUID = false;       //reset boolean
+  }
+  else characteristic.writeValue((byte)0x41); //send 0x41 if not valid hash
+}
+
+void addCharacteristics(BLEService service) {  //creates characteristics for each 16 byte chunck of the Hash value, reader number, and key characteristic
+  String tempServUUID = service.uuid();
+  tempServUUID.remove((tempServUUID.length() - 1));
+  tempServUUID.remove((tempServUUID.length() - 1));   //get service UUID excluding the last 2 HEX digits
+  for(int i = 0; i < 4; i++) {
+    String tempCharacUUID = tempServUUID + intToHEX(i); //define UUID for characterisitc that increments from the service UUID
+    BLEStringCharacteristic customCharacteristic(tempCharacUUID.c_str(), BLEWrite, 16); // Define a custom characteristic
+    customCharacteristic.writeValue("0000000000000000"); // Initialize the characteristic value
+    customCharacteristic.setEventHandler(BLEWritten, writeHandle);    //set event handler for when the characteristics are written to
+    service.addCharacteristic(customCharacteristic);  //add characteristic to service
+  }
+
+  String tempCharacUUID = tempServUUID + intToHEX(4); 
+  BLEStringCharacteristic customCharacteristic(tempCharacUUID.c_str(), BLEWrite, 1); //create fifth characteristic to send the reader number to know which file to check against
+  customCharacteristic.writeValue("0");
+  customCharacteristic.setEventHandler(BLEWritten, writeHandle);
+  service.addCharacteristic(customCharacteristic);
+
+  tempServUUID += intToHEX(5);
+  BLEByteCharacteristic keyCharacteristic(tempServUUID.c_str(), BLERead); //create a sixth characteristic that will act as the key characteristic signifying
+  keyCharacteristic.writeValue((byte)0x41);                               //an access grant or deny
+  keyCharacteristic.setEventHandler(BLERead, readHandle);
+  service.addCharacteristic(keyCharacteristic);
+}
+
+void BLE_Peripheral_Reset_Services() {  //resets BLE and adds services and characteristics per reader
   BLE.end();
   BLE.begin();
   BLE.setLocalName("EZALServer");  // Set the local name advertised by the server
 
   String RCount = readSDLine("RCOUNT.txt", 1);
   int ReaderCount = RCount.toInt();
-  for(int i = 1; i <= ReaderCount; i++) {
+  for(int i = 1; i <= ReaderCount; i++) {   
     String readerUUID = default_uuid;
-    readerUUID.remove(0,1);           
-    readerUUID = String(i, HEX) + readerUUID; //set proper UUID for designated reader 
-    String fileName = "READERS/UID" + String(i) + ".txt";
-    BLEService service(readerUUID.c_str());   //set defualt service UUID to reader UUID
-    addCharacteristicsFromFile(service, fileName);  //add characteristics from corresponding UID file
-    BLE.addService(service);  //add service to BLE service list
+    readerUUID.remove(0,1);
+    readerUUID = String(i, HEX) + readerUUID;
+    BLEService service(readerUUID.c_str());   //create services for each reader
+    addCharacteristics(service);              //create the characteristics for each service
+    BLE.addService(service);                  //add service
   }
 
   BLE.advertise(); // Start advertising
   Serial.println("Service reset complete");
 }
-
